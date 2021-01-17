eb90eab00f4c1a9fa3d28b9bdce265fc7af84a76
Dillon O'Connor <dilong818@gmail.com>
2021-01-16 22:29:42 -0500

    actually fix popular scope

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -8,5 +8,5 @@ class Playlist < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   scope :chronological, -> { order("created_at desc") }
-  scope :popular, -> { left_joins(:likes).select('playlists.*, COUNT(playlists.id) AS likes_count').group('likes.id').order('likes_count ASC') }
+  scope :popular, -> { left_joins(:likes).select('playlists.*, COUNT(playlist_id) AS likes_count').group('likes.id').order('likes_count DESC') }
 end
