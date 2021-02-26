fc20b5e5f560cffc0cdb2ab35416a34bb3382603
Dillon O'Connor <dilong818@gmail.com>
2021-02-25 22:13:21 -0500

    pg fix for likes destroy action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,7 +8,7 @@ class LikesController < ApplicationController
   end
   
   def destroy
-    @like = current_user.likes.find_by(params[:playlist_id])
+    @like = current_user.likes.find_by(playlist_id: params[:playlist_id])
     @like.destroy
     redirect_to root_url
   end
