7aa42803a2c1be88edfc4c10190ed5981ff4cdae
Dillon O'Connor <dilong818@gmail.com>
2021-03-01 21:27:22 -0500

    implement beta mobile menu

---
+++ app/javascript/packs/nav.js
@@ -0,0 +1,11 @@
+window.addEventListener("turbolinks:load", () => {
+  const hamburger = document.querySelector(".navbar-burger");
+  const menu = document.querySelector(".navbar-menu");
+
+  const expandMenu = () => {
+    hamburger.classList.toggle("is-active")
+    menu.classList.toggle("is-active");
+  }
+  hamburger.addEventListener("click", expandMenu);
+});
+
